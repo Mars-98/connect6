a48a973d96c6a5008f6a8698cf2c3ce6eb321f79
mary <adams222@uwm.edu>
2020-02-21 08:49:02 -0600

    test commit

---
+++ connect6.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def creatBoard():
+    board = np.zeros((7, 9))
+    return board
+
+    board = createBoard()
+    print(board)
+    print("hello")
+
+
+def main():
+    print("why")
