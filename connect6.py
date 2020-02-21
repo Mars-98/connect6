f6d8d1d6df9a0fcd80a519f8d3978e03d25191d0
mary <adams222@uwm.edu>
2020-02-21 09:56:40 -0600

    commit 1, made a matrix and did input validation, have skeleton of methods that I may need

--- connect6.py
+++ connect6.py
@@ -1,14 +1,43 @@
 import numpy as np
 
+ROW_CNT = 9
+COLUMN_CNT = 8
 
-def creatBoard():
-    board = np.zeros((7, 9))
+
+def createBoard():
+    board = np.zeros((8, 9), dtype=int)
     return board
 
-    board = createBoard()
-    print(board)
-    print("hello")
+
+def choosePiece():
+    pass
 
 
-def main():
-    print("why")
+def validPosition():
+    pass
+
+
+def nextOpenRow():
+    pass
+
+
+def printBoard(board):
+    pass
+
+
+board = createBoard()
+print(board)
+gameEnd = False
+turn = 0
+
+while not gameEnd:
+    # player one
+    if turn == 0:
+        col = int(input("Player 1: Select which circle you want to: 0-8:  "))
+
+    # player two
+    else:
+        col = int(input("Player 2: Select which circle you want to: 0-8:  "))
+
+    turn += 1  # increments to 1 for player 2's turn
+    turn %= 2  # maintains turn to stay between 0 and 1
