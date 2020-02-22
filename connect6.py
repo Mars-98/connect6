1880ac3eb70e09b39f1259b35234f47f6748b93a
mary <adams222@uwm.edu>
2020-02-21 21:35:04 -0600

    Finished the logic

--- connect6.py
+++ connect6.py
@@ -1,9 +1,8 @@
 import numpy as np
+from soupsieve.util import string
 
 ROW_CNT = 9
 COLUMN_CNT = 8
-TURN = 0
-FIRST_TURN = 0
 
 
 def createBoard():
@@ -14,40 +13,48 @@ def createBoard():
 board = createBoard()
 print(board)
 gameEnd = False
+turn = 0
 
 
-def choosePiece():
-    pass
+def dropPiece(Board, Row, Col, playerPiece):  # drop the piece in the spot the player has chosen
+    Board[Row][Col] = playerPiece
 
 
-def validPosition():
-    pass
+def validLoc(Board, Col):  # checks if the position we chose is valid for input
+    if Board[7][Col] == 0:
+        return True
+    return False
 
 
-def nextOpenRow():
-    pass
+def nextOpenRow(Board, Col):  # gets the next open row on the column we're on
+    for Row in range(ROW_CNT):
+        if Board[Row][Col] == 0:
+            return Row
 
 
-# def changeTurn(turn=TURN):
-#     if firstT == 0:
-#         return 0
-#     turn += 1  # increments to 1 for player 2's turn
-#     turn %= 2  # maintains turn to stay between 0 and 1
-#     firstT += 1
-#     return turn
+def changeTurn(Turn):
+    Turn += 1  # increments to 1 for player 2's turn
+    Turn %= 2  # maintains turn to stay between 0 and 1
+    return Turn
 
 
-def printBoard(board):
-    pass
+def printBoard(Board):
+    print(np.flip(Board, 0))
 
 
 while not gameEnd:
-    # player one
-    if TURN == 0:  # changeTurn(FIRST_TURN, TURN) == 0:
-        col = int(input("Player 1: Select which circle you want to: 0-8:  "))
+    # name = input("Lets get started! What's your name? ")
 
+    # player one
+    if turn == 0:
+        player = string(1)
     # player two
     else:
-        col = int(input("Player 2: Select which circle you want to: 0-8:  "))
+        player = string(2)
 
-# changeTurn(FIRST_TURN, TURN);
+    turn = changeTurn(turn)
+    col = int(input("Player " + player + ": Select which position you want to fill: 0-8:  "))
+    if validLoc(board, col):
+        row = nextOpenRow(board, col)
+        dropPiece(board, row, col, player)
+    printBoard(board)
